75d24117485ac2a4b67ce5c9d7c02ce78237c5e5
Eric Bailey <Eric@Erics-MacBook-Pro.local>
2014-09-05 10:01:11 -0500

    Quick Update - Pre-productution
    Added the beginnings of a grid. Still need to edit typographical
    hierarchy, among other things.

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php include('head.php'); ?>
 
 
-<!--    build page here-->
+    <div class="container">
+        <div class="test row">
+            <div class="column--test column sm_size1of1 xl_size1of3"></div>
+            <div class="column--test column sm_size1of1 xl_size1of3"></div>
+            <div class="column--test column sm_size1of1 xl_size1of3"></div>
+        </div>
+    </div>
 
 
 <?php include('footer.php'); ?>
