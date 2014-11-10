6be0ce45fd44d6d4641cefadaa965c9567b224dd
Eric Bailey <Eric@Erics-MacBook-Pro-8.local>
2014-11-10 10:01:21 -0600

    svbstrate v0.4.1
    In the process of updating structure, cutting fat, and restyling
    elements for a more holistic framework and brand.

--- footer.php
+++ footer.php
@@ -1,5 +1,15 @@
-<footer>
-<!--    insert footer content here-->
+<footer id="footer" class="container">
+    <div class="row">
+        <div class="column lg_size1of2 size1of1">
+            <button class="button--sm u-inlineRelative"><i class="fa fa-twitter fa-fw"></i>&nbsp;Tweet</button>
+<!--            <button class="button--sm button--ghost u-inlineRelative">Docs</button>-->
+            <button class="button--sm button--ghost u-inlineRelative"><a href="#0">Download</a></button>
+            <button class="button--sm button--ghost u-inlineRelative"><a href="#0">Github</a></button>
+        </div>
+        <div class="column lg_size1of2 size1of1 u-rightAlign">
+            <h5>Made with <i class="fa fa-heart fa-fw"></i> and curiosity by <a href="http://ericbailey.co">estrattonbailey</a>.</h5>
+        </div>
+    </div>
 </footer>
 
 
--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php include('header.php'); ?>
 
-<main>
-    <section id="top" class="u-height3of4 container">
+    <section id="top" class="container">
         <div class="row">
             <div class="column lg_size1of2 size1of1">
-                <h1>SVBSTRATE</h1>
-                <h4 class="column lg_size3of4 size1of1">Basic starter templates for SUIT CSS inspired SASS projects.</h4>
+                <h1 class="h0">SVBSTRATE</h1>
+                <h4 class="column lg_size3of4 size1of1">Base-level SCSS framework for OOCSS projects.</h4>
             </div>
             <div id="mainSocialIcons" class="lg_u-rightAlign column lg_size1of2 size1of1">
                 <h2><a class="shareIcon" href="https://twitter.com/home?status=Check%20out%20SVBSTRATE,%20a%20super-basic%20SASS%20framework%20by%20@estrattonbailey.%20http://svbstrate.io" target="_blank"><i class="fa fa-twitter fa-fw"></i></a></h2>
@@ -13,102 +12,35 @@
             </div>
         </div>
     </section>
+
     <section id="intro" class="container">
         <div class="row">
-            <div class="column lg_size1of2 size1of1 lg_suffix1of2 suffix0of0">
-                <h2>THE GOODS:</h2>
-                <ul>
-                    <li><h4><a href="http://sass-lang.com/" target="_blank">SASS</a> (fuck yeah)</h4></li>
-                    <li><h4>super-light <code>inline-block</code> based grid system</h4></li>
-                    <li><h4>basic golden typographical hierarchy</h4></li>
-                    <li><h4><a href="http://fortawesome.github.io/Font-Awesome/icons/" target="_blank">FontAwesome</a></h4></li>
-                    <li><h4>7 included stock images from one of my side projects, <a href="http://startupstockphotos.com" target="_blank">Startup Stock Photos</a></h4></li>
-                </ul>
+            <h1 class="column">An OOCSS framework built for designers.</h1>
+            <span class="divider column size1of1 med_size1of3 med_suffix2of3"></span>
+            <h4 class="column size1of1 med_size1of2">Svbstrate is a collection of basic styles, inline-block block based layout modules, and simple utilities for a fast, simple way to move from prototype to production.</h4>
+        </div>
+    </section>
+
+    <section class="example container">
+        <div class="row u-verticalAlign--top">
+            <div class="column size1of1 med_size1of2">
+                <h2 class="column">Built for Designers</h2>
+                <p>Focus on the design, not the code. Easy to use naming conventions make protyping basic layouts, typography, and colors a snap.</p>
             </div>
-            <div class="column lg_size1of2 size1of1 lg_suffix1of2 suffix0of0">
-                <h3>PLUS A COUPLE FAVS:</h3>
-                <ul>
-                    <li><h4><a href="https://github.com/Prinzhorn/skrollr" target="_blank">Skrollr</a> Parallax Library</h4></li>
-                    <li><h4>the super-neat <a href="https://github.com/julianlloyd/scrollReveal.js" target="_blank">ScrollReveal</a> Library</h4></li>
-                </ul>
+            <div class="column size1of1 med_size1of2">
+                <h2 class="column">Built for Developers</h2>
+                <p>Svbstrate is built for speed and designed to stay out of the way. Mockup and scale with SUIT CSS inspired naming conventions and a focus on module based layouts.</p>
+            </div>
+            <div class="column size1of1 med_size1of2">
+                <h2 class="column">Design in the Browser</h2>
+                <p>Produce clean, minimally styled designs quickly with Svbstrate's simple classes and utitilites. Then, add your own styles to refine.</p>
+            </div>
+            <div class="column size1of1 med_size1of2">
+                <h2 class="column">Maintainable RWD</h2>
+                <p>Define breakpoints and adjust layouts via separate stylesheets for more maintainable code and less compiled CSS.</p>
             </div>
         </div>
     </section>
-    <section class="container">
-        <div class="row">
-             <div class="column lg_size1of2 size1of1">
-                <h2>GET SASSY</h2>
-                 <h4>It's the best (imo). Be smart, use it for variables, <code>calc()</code>, etc, but keep the nesting and functions to a minimum. Ain't nobody got time for that.<br/>
-                    <br/>
-                     Also, SVBSTRATE is mobile first. Use the separate .scss files in <code>~/components/</code> for the different breakpoints. Yay minimal CSS.<br/>
-                    <br/>
-                     Run sass <code>--watch scss style.scss:style.css</code> to compile from terminal.</h4>
-            </div>
-        </div>
-    </section>
-    <section id="gridSection" class="container">
-        <div class="row">
-            <div class="column size1of1">
-                <h2>INLINE-BLOCK GRID</h2>
-                <h4 class="column lg_size1of2 size1of1">Floats are silly. Inline-block based grids keep it simple and use easy to understand syntax. That's smart. Adjust settings in <code>~/components/_grid--settings.scss</code>, including breakpoints.</h4>
-            </div>
-        </div>
-        <div class="row">
-            <div class="block column lg_size1of4 size1of2"><h4>lg_size1of4 size1of2</h4></div>
-            <div class="block column lg_size1of4 size1of2"><h4>lg_size1of4 size1of2</h4></div>
-            <div class="block column lg_size1of4 size1of2"><h4>lg_size1of4 size1of2</h4></div>
-            <div class="block column lg_size1of4 size1of2">
-                <h4>lg_size1of4 size1of2</h4>
-                <div class="block--nested block column lg_size1of2 size1of1"><h5>lg_size1of2 size1of1</h5></div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="column size1of1">
-                <h3>VERTICAL ALIGNMENT</h3>
-                <h4 class="column lg_size1of2 size1of1">The row container below has a class of <code>u-verticalAlign--middle</code>, forcing the child blocks to align vertically. Use <code>u-verticalAlign--top</code> <code>u-verticalAlign--bottom</code> for top and bottom alignment.</h4>
-            </div>
-        </div>
-        <div id="verticalAlign" class="verticalAlign--middle row">
-            <h4>u-verticalAlign--middle row</h4>
-            <div class="block--nested block column size1of4"></div>
-            <div class="block column size1of4"></div>
-            <div class="block--nested block column size1of4"></div>
-            <div class="block column size1of4"></div>
-        </div>
-    </section>
-    
-    <!-- outer slide container-->
-    <section class="sliderContainer">
-        
-        <div class="js-slider">
-            
-            <div class="slide">
-                <img src="resources/images/1.jpg" alt="" />
-            </div>
-            
-            <div class="slide">
-                <img src="resources/images/2.jpg" alt="" />
-            </div>
-            
-        </div>
-        
-        <nav class="slider-navigation">
-            <div class="next"><i class="fa fa-chevron-right fa-3x fa-fw"></i></div>
-            <div class="prev"><i class="fa fa-chevron-left fa-3x fa-fw"></i></div>
-        </nav>
-        
-    </section>
-    <!-- end slide container-->
-    
-    <section id="footer" class="container">
-        <div class="row">
-            <div class="column lg_size1of2 size1of1">
-                <h4>And that's it. Think you can make it better? I'm sure you can. <a href="https://github.com/estrattonbailey/svbstrate" target="_blank">Fork me.</a> I'd love to learn something new.</h4><br/><br/>
-                <h4>Made with love and curiosity by <a href="http://twitter.com/estrattonbailey" target="_blank">@estrattonbailey</a>.</h4>
-            </div>
-        </div>
-    </section>
-</main>
 
 
 <?php include('footer.php'); ?>
