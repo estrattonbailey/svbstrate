a8b2db7401df6b5e48e9bdce9f4294df6fca2330
Eric Bailey <Eric@Erics-MacBook-Pro-8.local>
2014-11-10 10:14:23 -0600

    svbstrate v0.4.1

--- README.md
+++ README.md
@@ -1,31 +1,7 @@
 # SVBSTRATE
 
-## Basic starter templates for SUIT CSS inspired SASS projects.
+## Base-level SCSS framework for OOCSS projects.
 
 ### [Demo](http://svbstrate.io)
-
-Only the necessities:
-- SASS
-- customizable inline-block grid
-- typographical hierarchy
-- FontAwesome
-- 7 included stock images from one of my side projects, [Startup Stock Photos](http://startupstockphotos.com)
-
-Plus two favorites:
-- [Skrollr](https://github.com/Prinzhorn/skrollr) parallax library
-- the super-neat [ScrollReveal](https://github.com/julianlloyd/scrollReveal.js) library
-
-####SASS
-It's the best. Be smart, use it for variables, calc(), etc, but keep the nesting and functions to a minimum. Ain't nobody got time for that.
-
-Also, SVBSTRATE is mobile first. Use the separate ```*.scss``` files in ```~/partials/*``` for the different breakpoints. Yay minimal CSS.
-
-Run ```sass --watch scss style.scss:style.css``` to compile from terminal.
-
-####Inline Block Grid
-Floats are silly. Inline-block based grids keep it simple and use easy to understand syntax. That's smart. Adjust settings in ```~/components/_grid--settings.scss```, including breakpoints.
-
-* * *
-And that's it. Think you can make it better? I'm sure you can. **Fork me.** I'd love to learn something new.
 
 Created with love and curiosity by [@estrattonbailey](http://twitter.com/estrattonbailey).
--- footer.php
+++ footer.php
@@ -1,10 +1,16 @@
 <footer id="footer" class="container">
     <div class="row">
         <div class="column lg_size1of2 size1of1">
-            <button class="button--sm u-inlineRelative"><i class="fa fa-twitter fa-fw"></i>&nbsp;Tweet</button>
+            <a href="https://twitter.com/home?status=Check%20out%20SVBSTRATE,%20a%20super-basic%20SASS%20framework%20by%20@estrattonbailey.%20http://svbstrate.io" target="_blank">
+                <button class="button--sm u-inlineRelative"><i class="fa fa-twitter fa-fw"></i>&nbsp;Tweet</button>
+            </a>
 <!--            <button class="button--sm button--ghost u-inlineRelative">Docs</button>-->
-            <button class="button--sm button--ghost u-inlineRelative"><a href="#0">Download</a></button>
-            <button class="button--sm button--ghost u-inlineRelative"><a href="#0">Github</a></button>
+            <a href="https://github.com/estrattonbailey/svbstrate/blob/master/svbstrate_v0.4.1_complete.zip?raw=true">
+                <button class="button--sm button--ghost u-inlineRelative">Download</button>
+            </a>
+            <a href="https://github.com/estrattonbailey/svbstrate">
+                <button class="button--sm button--ghost u-inlineRelative">Github</button>
+            </a>
         </div>
         <div class="column lg_size1of2 size1of1 u-rightAlign">
             <h5>Made with <i class="fa fa-heart fa-fw"></i> and curiosity by <a href="http://ericbailey.co">estrattonbailey</a>.</h5>
