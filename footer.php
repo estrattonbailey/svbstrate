e011e8c9e2754fa5c3522c3d21eeaf74a63a9668
Eric Bailey <ericstrattonbailey@gmail.com>
2015-01-07 14:58:58 -0500

    svbstrate v0.5

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
             <a href="https://twitter.com/home?status=Check%20out%20SVBSTRATE,%20a%20super-basic%20SASS%20framework%20by%20@estrattonbailey.%20http://svbstrate.io" target="_blank">
                 <button class="button--sm u-inlineRelative"><i class="fa fa-twitter fa-fw"></i>&nbsp;Tweet</button>
             </a>
-            <a href="https://github.com/estrattonbailey/svbstrate/blob/master/svbstrate_v0.4.1_complete.zip?raw=true">
+            <a href="https://github.com/estrattonbailey/svbstrate/blob/master/svbstrate_v0.4.2_complete.zip?raw=true">
                 <button class="button--sm button--ghost u-inlineRelative">Download</button>
             </a>
             <a href="https://github.com/estrattonbailey/svbstrate">
@@ -17,21 +17,5 @@
     </div>
 </footer>
 
-
-
-
-    <!-- ScrollReveal is neat: https://github.com/julianlloyd/scrollReveal.js -->
-    <script src='js/scrollReveal.js'></script>
-
-    <!-- Superslides slideshow plugin: https://github.com/nicinabox/superslides -->
-    <script src='js/superslides.js'></script>
-
-    <!-- Project javascript-->
-    <script src='js/scripts.js'></script>
-
-    <!-- Instantiate Superslides plugin-->
-    <script>
-        $('.sliderContainer').superslides();
-    </script>
 
 </body></html>
