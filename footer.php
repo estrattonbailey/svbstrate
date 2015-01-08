c0a1d6f414283d6a4da50ad5dd182aa278ab254a
Eric Bailey <ericstrattonbailey@gmail.com>
2015-01-08 16:04:46 -0500

    svbstrate v0.5.1

--- footer.php
+++ footer.php
@@ -2,13 +2,13 @@
     <div class="row">
         <div class="column lg_size1of2 size1of1">
             <a href="https://twitter.com/home?status=Check%20out%20SVBSTRATE,%20a%20super-basic%20SASS%20framework%20by%20@estrattonbailey.%20http://svbstrate.io" target="_blank">
-                <button class="button--sm u-inlineRelative"><i class="fa fa-twitter fa-fw"></i>&nbsp;Tweet</button>
+                <button class="button--sm button-inline button-invert"><i class="fa fa-twitter fa-fw"></i>&nbsp;Tweet</button>
             </a>
             <a href="https://github.com/estrattonbailey/svbstrate/blob/master/svbstrate_v0.5_complete.zip?raw=true">
-                <button class="button--sm button--ghost u-inlineRelative">Download</button>
+                <button class="button--sm button-ghost button-invert button-inline">Download</button>
             </a>
             <a href="https://github.com/estrattonbailey/svbstrate">
-                <button class="button--sm button--ghost u-inlineRelative">Github</button>
+                <button class="button--sm button-ghost button-invert button-inline">Github</button>
             </a>
         </div>
         <div class="column lg_size1of2 size1of1 u-rightAlign">
--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php include('header.php'); ?>
 
-    <section id="top" class="container">
+    <section class="hero color-tertiary container">
         <div class="row">
             <div class="column lg_size1of2 size1of1">
                 <h1 class="h0">SVBSTRATE</h1>
-                <h4 class="column lg_size3of4 size1of1">Base-level SCSS framework for OOCSS projects.</h4>
+                <h4 class="column lg_size3of4 size1of1">Base-level SCSS framework for making websites.</h4>
             </div>
             <div id="mainSocialIcons" class="lg_u-rightAlign column lg_size1of2 size1of1">
                 <h2><a class="shareIcon" href="https://twitter.com/home?status=Check%20out%20SVBSTRATE,%20a%20super-basic%20SASS%20framework%20by%20@estrattonbailey.%20http://svbstrate.io" target="_blank"><i class="fa fa-twitter fa-fw"></i></a></h2>
@@ -13,23 +13,23 @@
         </div>
     </section>
 
-    <section id="intro" class="container">
+    <section id="intro" class="container container--padded">
         <div class="row">
-            <h1 class="column">An OOCSS framework built for designers.</h1>
+            <h1 class="column">Skip the grunt work.</h1>
             <span class="divider column size1of1 med_size1of3 med_suffix2of3"></span>
-            <h4 class="column size1of1 med_size1of2">Svbstrate is a collection of basic styles, inline-block block based layout modules, and simple utilities for a fast, simple way to move from prototype to production.</h4>
+            <h4 class="column size1of1 med_size1of2">Svbstrate is a foundation of styles + a grid framework for devs who don't want to repeat themselves, but don't need all the fancy-shmancy stuff that's you might find in other SASS base-level frameworks.</h4>
         </div>
     </section>
 
-    <section class="example container">
+    <section class="example container container--padded">
         <div class="row u-verticalAlign--top">
             <div class="column size1of1 med_size1of2">
-                <h2 class="column">Built for Designers</h2>
-                <p>Focus on the design, not the code. Easy to use naming conventions make protyping basic layouts, typography, and colors a snap.</p>
+                <h2 class="column">Built Easy</h2>
+                <p>Focus on making it look good, not the code. Easy to use naming conventions make protyping basic layouts, typography, and colors a snap.</p>
             </div>
             <div class="column size1of1 med_size1of2">
-                <h2 class="column">Built for Developers</h2>
-                <p>Svbstrate is built for speed and designed to stay out of the way. Mockup and scale with SUIT CSS inspired naming conventions and a focus on module based layouts.</p>
+                <h2 class="column">Built Well</h2>
+                <p>Svbstrate is built simply (read, fast) and designed to stay out of the way. Mockup and scale your sites with SUIT CSS inspired naming conventions and a focus on modular layouts.</p>
             </div>
             <div class="column size1of1 med_size1of2">
                 <h2 class="column">Design in the Browser</h2>
@@ -37,7 +37,7 @@
             </div>
             <div class="column size1of1 med_size1of2">
                 <h2 class="column">Maintainable RWD</h2>
-                <p>Define breakpoints and adjust layouts via separate stylesheets for more maintainable code and less compiled CSS.</p>
+                <p>Define breakpoints and adjust layouts via separate stylesheets for more maintainable code.</p>
             </div>
         </div>
     </section>
