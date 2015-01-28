89cf396049768cae45b0be532e5cfcb33fd0c14b
Eric Bailey <ericstrattonbailey@gmail.com>
2015-01-27 21:16:18 -0500

    svbstrate v0.6
    A whole new ballgame.

--- index.php
+++ index.php
@@ -1,59 +1,5 @@
 <?php include('templates/header.php'); ?>
 
-    <div class="container pad-lg">
-    	<div class="row inline">
-    		<div class="block s12 test"></div>
-    		<div class="block s12 test"></div>
 
-    		<div class="block s13 test"></div>
-    		<div class="block s13 test"></div>
-    		<div class="block s13 test"></div>
-
-    		<div class="block s23 test"></div>
-    		<div class="block s13 test"></div>
-
-    		<div class="block s56 test"></div>
-    		<div class="block s16 test"></div>
-
-    		<div class="block s12 test"></div>
-    		<div class="block s12 test"></div>
-
-    		<div class="block s14 test"></div>
-    		<div class="block s14 test"></div>
-    		<div class="block s14 test"></div>
-    		<div class="block s14 test"></div>
-
-    		<div class="block s34 test"></div>
-    		<div class="block s14 test"></div>
-    	</div>
-    </div>
-
-    <div class="container pad-lg">
-    	<div class="row float">
-    		<div class="block s12 test"></div>
-    		<div class="block s12 test"></div>
-
-    		<div class="block s13 test"></div>
-    		<div class="block s13 test"></div>
-    		<div class="block s13 test"></div>
-
-    		<div class="block s23 test"></div>
-    		<div class="block s13 test"></div>
-
-    		<div class="block s56 test"></div>
-    		<div class="block s16 test"></div>
-
-    		<div class="block s12 test"></div>
-    		<div class="block s12 test"></div>
-
-    		<div class="block s14 test"></div>
-    		<div class="block s14 test"></div>
-    		<div class="block s14 test"></div>
-    		<div class="block s14 test"></div>
-
-    		<div class="block s34 test"></div>
-    		<div class="block s14 test"></div>
-    	</div>
-    </div>
 
 <?php include('templates/footer.php'); ?>
