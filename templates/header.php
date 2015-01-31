453f948416736688a2756963cb6aca94b6cb1e85
Eric Bailey <ericstrattonbailey@gmail.com>
2015-01-31 18:14:56 -0500

    Wrong imports

--- templates/header.php
+++ templates/header.php
@@ -4,25 +4,25 @@
 
 <head>
     <link rel="icon" type="image/ico" href="http://svbstrate.io/favicon.ico"/>
-    
+
     <meta charset="UTF-8"/>
     <!--[if lt IE 9]>
     <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
     <!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
-    
+
     <meta name="author" content="Eric Bailey">
 	<meta name="viewport" content="width=320, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
 	<meta name="keywords" content="SASS, project template, SUIT CSS"/>
 	<meta name="description" content="Basic starter templates SASS projects."/>
-    
+
         <!-- Open Graph data -->
 	<meta prefix="og: http://ogp.me/ns#" property="og:description" content="Basic starter templates SASS projects."/>
 	<meta prefix="og: http://ogp.me/ns#" property="og:site_name" content="SVBSTRATE"/>
     <meta prefix="og: http://ogp.me/ns#" property="og:title" content="SVBSTRATE" />
     <meta prefix="og: http://ogp.me/ns#" property="og:image" content="http://svbstrate.io/resources/images/ogImg.png" />
     <meta prefix="og: http://ogp.me/ns#" property="og:url" content="http://svbstrate.io/" />
-    
+
         <!-- Twitter Card data -->
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:site" content="http://svbstrate.io">
@@ -30,17 +30,17 @@
     <meta name="twitter:description" content="Basic starter templates SASS projects.">
     <meta name="twitter:creator" content="@estrattonbailey">
     <meta name="twitter:image:src" content="http://svbstrate.io/resources/images/ogImg.png">
-    
+
     <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-    
-    <link href="style.css" media="screen, projector, print" rel="stylesheet" type="text/css" /> 
-    
+
+    <link href="style.css" media="screen, projector, print" rel="stylesheet" type="text/css" />
+
         <!--    Fonts from TypeKit-->
-    <script src="//use.typekit.net/yyo3gqh.js"></script>
-    <script>try{Typekit.load();}catch(e){}</script>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,800' rel='stylesheet' type='text/css'>
+
         <!--    FontAwesome-->
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-    
+
     <title>SVBSTRATE</title>
 
 </head>
