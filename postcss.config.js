4246153b60bce2c3a7aa834330c18538e33b99a3
estrattonbailey <ericstrattonbailey@gmail.com>
2017-07-07 15:13:51 -0400

    Pare-down to minimum

--- postcss.config.js
+++ postcss.config.js
@@ -1,28 +1,9 @@
-const postcss = require('postcss')
-
-const output = process.env.NODE_ENV !== 'production' ? 'dist/svbstrate.css' : 'dist/svbstrate.min.css'
-const plugins = process.env.NODE_ENV !== 'production' ? [
-  'precss',
-  'postcss-import',
-  'postcss-custom-properties',
-  'postcss-calc',
-  'postcss-discard-comments',
-  'autoprefixer',
-  'postcss-reporter',
-  'postcss-prettify',
-] : [
-  'precss',
-  'postcss-import',
-  'postcss-custom-properties',
-  'postcss-calc',
-  'postcss-discard-comments',
-  'autoprefixer',
-  'postcss-reporter',
-  'cssnano',
-]
+const p = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  use: plugins,
-  input: 'src/main.css',
-  output: output
+  plugins: [
+    require('postcss-import'),
+    require('cssnano')
+  ]
 }
+
